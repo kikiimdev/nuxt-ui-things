b803aca0c20a206e1259346b3bfde1185272e3d6
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2024-08-31 15:32:38 +0800

    feat: add UiAppSection

---
+++ app/components/Ui/App/Container.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+    //
+</script>
+
+<template>
+    <div class="flex flex-col gap-5 md:flex-row md:items-center md:justify-between">
+        <slot />
+    </div>
+</template>
