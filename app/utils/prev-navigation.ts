25eeec4a98324f3bd52ac1b8da439880dd3d1a42
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2024-08-31 13:27:14 +0800

    fix: router.back()

--- app/utils/prev-navigation.ts
+++ app/utils/prev-navigation.ts
@@ -2,5 +2,5 @@ export const prevNavigation = () => {
     if (import.meta.server) return
 
     const router = useRouter()
-    window.history.length > 1 ? router.go(-1) : router.push('/app')
+    window.history.length > 1 ? router.back() : router.push('/app')
 }
